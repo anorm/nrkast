202786b2b821ebed99f348b1b8789df87624a414
Anders Norman <anders@norman.info>
2024-07-25 16:18:46 +0200

    Added scrape endpoint

--- nrkast/server.py
+++ nrkast/server.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from contextlib import asynccontextmanager
 
 from . import factory
 
-app = FastAPI()
+scraper = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with factory.produce("scraper") as s:
+        global scraper
+        scraper = s
+        yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/ping")
 def ping():
     return "OK"
+
+@app.get("/{slug}")
+async def scrape(slug):
+    body = await scraper.scrape(slug)
+    return Response(content=body, media_type="text/xml")
