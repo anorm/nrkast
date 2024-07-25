5aafc80e1ed99f46b7c90cf32c9c3c2f23f79ec1
Anders Norman <anders@norman.info>
2024-07-25 16:06:54 +0200

    Cleanup

--- entry-point.sh
+++ entry-point.sh
@@ -3,6 +3,5 @@
 set -e
 
 export YOYO_DATABASE=$(echo $DB_CONNECTION | sed -e "s/postgresql:/postgresql+psycopg:/g")
-echo $YOYO_DATABASE
 yoyo apply
 uvicorn --host 0.0.0.0 --port $PORT nrkast.server:app
