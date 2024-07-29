2b76f696f54601eac0c653e2ef5b12e6a983cd37
Anders Norman <anders@norman.info>
2024-07-29 09:18:16 +0200

    Cross build

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,6 +6,13 @@ REGISTRY=docker.n4n.no
 IMAGE=nrkast
 TAG=$1
 
+docker buildx build \
+    --platform linux/amd64 \
+    --push \
+    -t $REGISTRY/$IMAGE:latest \
+    -t $REGISTRY/$IMAGE:$TAG \
+    .
+exit
 docker build . -t $REGISTRY/$IMAGE:latest -t $REGISTRY/$IMAGE:$TAG
 docker push $REGISTRY/$IMAGE:latest
 docker push $REGISTRY/$IMAGE:$TAG
